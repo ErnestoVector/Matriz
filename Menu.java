94b0bd21a349a6f8a04148555ac40278298ca404
Lalo <lalo.d3veloper@gmail.com>
2017-06-19 01:39:02 -0500

    Añadi la funcion LimpiarPantalla en la clase Menu

--- Menu.java
+++ Menu.java
@@ -2,8 +2,9 @@
 
 public class Menu
 {
-	public void imprimeMenu()
-	{
+	public static void imprimeMenu()
+	{	
+				
 		System.out.println("\nMenu:");
 		System.out.println("\t1) Matriz A (Llena matriz).");
 		System.out.println("\t2) Matriz B (Llena matriz).");
@@ -14,5 +15,16 @@ public class Menu
 		System.out.println("\t7) kB");
 		System.out.println("\t8) Traza A");
 		System.out.println("\t9) Traza B");
+		System.out.println("\t0) Para salir");
+
 	}
+
+	public static void LimpiarPantalla()
+	{
+		System.out.print("\n\nPresiona cualquier tecla para elegir otra opcion");
+		new java.util.Scanner(System.in).nextLine();
+		System.out.print("\033[H\033[2J");
+		System.out.flush();			
+	}
+	
 }
